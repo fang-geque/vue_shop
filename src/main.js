0ca8d8fbee7c1e4cdf70c4ce36e6b80a2558510a
wangshun <513726532@qq.com>
2019-12-26 17:43:43 +0800

    完成了登录

--- src/main.js
+++ src/main.js
@@ -3,6 +3,23 @@ import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
 
+//完整引入element-ui
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
+
+//导入字体图标
+import '@/assets/fonts/iconfont.css'
+
+//导入全局样式表
+import '@/assets/css/global.css'
+
+//配置请求的根路径
+import axios from 'axios'
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+Vue.prototype.$http = axios
+
+
 Vue.config.productionTip = false
 
 new Vue({
